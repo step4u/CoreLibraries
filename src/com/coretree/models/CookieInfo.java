aafd121c7be3c0ff713efbea188cc5740a642065
sequel2 <sequel2@RD0>
2016-01-10 23:38:48 +0900

    2016-01-10 11:38
    Signed-off-by: sequel2 <sequel2@RD0>

--- src/com/coretree/models/CookieInfo.java
+++ src/com/coretree/models/CookieInfo.java
@@ -1,8 +1,12 @@
 package com.coretree.models;
 
 public class CookieInfo {
+	private String username;
 	private String ext;
 	private String role;
+	
+	public void setUsername(String username) { this.username = username; }
+	public String getUsername() { return this.username; }
 	
 	public void setExt(String ext) { this.ext = ext; }
 	public String getExt() { return this.ext; }
