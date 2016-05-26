22fd675b437b2c47c468dd831b7e61485960ee3e
JC LEE <step4u@gmail.com>
2016-05-26 17:40:55 +0900

    2016-05-26 17:40
    1. DiskFreeSpaceTest
    
    Signed-off-by: JC LEE <step4u@gmail.com>

---
+++ src/com/coretree/core/DiskSpaceTest.java
@@ -0,0 +1,15 @@
+package com.coretree.core;
+
+import java.io.File;
+
+public class DiskSpaceTest {
+
+	public static void main(String[] args) {
+        System.out.println(args[0]);
+    	File file = new File(args[0]);
+        long freespace = file.getFreeSpace(); 
+
+        System.out.println("Disk Free Space (GB):" + freespace / 1024 / 1024);
+	}
+
+}
